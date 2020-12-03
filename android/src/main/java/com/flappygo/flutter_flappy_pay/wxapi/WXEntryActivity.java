158e66604abe4545ebe332a5a1cb7688f410e528
lijunlin <327603258@qq.com>
2020-12-03 10:32:50 +0800

    完善微信回调桥接

---
+++ android/src/main/java/com/flappygo/flutter_flappy_pay/wxapi/WXEntryActivity.java
@@ -0,0 +1,88 @@
+package com.flappygo.flutter_flappy_pay.wxapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.chinaums.pppay.unify.UnifyPayPlugin;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelpay.PayResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+    private static final String TAG = "WXPayEntryActivity";
+    //微信支付
+    private IWXAPI wxApi = null;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        wxApi = WxRegister.getWxAPi();
+        wxApi.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        wxApi.handleIntent(intent, this);
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+        Log.e(TAG,"req");
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+
+        //支付结果接收
+        if (baseResp.getType() == ConstantsAPI.COMMAND_LAUNCH_WX_MINIPROGRAM) {
+            UnifyPayPlugin.getInstance(this).getWXListener().onResponse(this, baseResp);
+        }
+
+        Log.e(TAG,"微信支付回调");
+        if (WxRegister.getCallback() == null) {
+            Log.d(TAG, "CallbackContext 无效");
+            startMainActivity();
+            return ;
+        }
+
+        Map<String, String> resultMap = new HashMap<String, String>();
+        resultMap.put("errCode",""+baseResp.errCode);
+        resultMap.put("errStr",baseResp.errStr);
+        resultMap.put("transaction",baseResp.transaction);
+        resultMap.put("openId",baseResp.openId);
+
+        switch (baseResp.getType()){
+            case ConstantsAPI.COMMAND_PAY_BY_WX: {
+                PayResp wxPayResp = (PayResp) baseResp;
+                resultMap.put("prepayId", wxPayResp.prepayId);
+                resultMap.put("extData", wxPayResp.extData);
+                resultMap.put("returnKey", wxPayResp.returnKey);
+            }
+            break;
+        }
+
+        Log.d(TAG,"wechat return ::" + resultMap.toString());
+        JSONObject jsonObject = new JSONObject(resultMap);
+        WxRegister.getCallback().success(jsonObject.toString());
+
+        finish();
+    }
+
+    protected void startMainActivity() {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(intent);
+    }
+}
